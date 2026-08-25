d46838f0669fe8ab5ab71a17e0639d5636a32c67
kimdt27 <kt@easv.dk>
2026-08-25 20:11:33 +0200

    Basics

---
+++ basic/func.php
@@ -0,0 +1,13 @@
+<?php
+echo "JUHUUU!!!";
+$functions = get_defined_functions();
+echo count($functions['internal']);
+echo "<br>";
+//var_dump($functions);
+
+$intfuncs = $functions['internal'];
+
+foreach($intfuncs as $func){
+echo $func."<br>";
+}
+?>
